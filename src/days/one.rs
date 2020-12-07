3a071a2d13ced02bba4c67f8a6214d2f7925d6dc
Ruud Linssen <r.p.j.linssen@gmail.com>
2020-12-07 20:33:02 +0100

    Fixed day 1

--- src/days/one.rs
+++ src/days/one.rs
@@ -5,13 +5,23 @@ static FILENAME: &str = "inputs/inputday1";
 
 pub fn solve() {
     let input = fs::read_to_string(FILENAME).unwrap();
-    if let Some(answer) = input
+
+    println!(
+        "Answer to day 1 part 1 is {}",
+        get_solution(&input, 2).unwrap()
+    );
+
+    println!(
+        "Answer to day 1 part 2 is {}",
+        get_solution(&input, 3).unwrap()
+    );
+}
+
+fn get_solution(input: &str, combs: usize) -> Option<u32> {
+    input
         .lines()
-        .filter_map(|c| c.parse::<i32>().ok())
-        .combinations(2)
+        .filter_map(|c| c.parse().ok())
+        .combinations(combs)
         .find(|comb| comb.iter().fold(0, |acc, x| acc + x) == 2020)
         .map(|comb| comb.iter().fold(1, |acc, x| acc * x))
-    {
-        println!("Answer to day 1 part 1 is {}", answer);
-    }
 }

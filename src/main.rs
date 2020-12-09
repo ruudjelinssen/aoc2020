40a42144e979b2fbfd2d11ff22ab7fdf6e5a4b96
Ruud Linssen <r.p.j.linssen@student.tue.nl>
2020-12-09 12:37:25 +0100

    Running all in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod days;
 
 fn main() {
-    // days::one::solve();
-    // days::two::solve();
-    // days::three::solve();
-    // days::four::solve();
-    // days::five::solve();
-    // days::six::solve();
-    // days::seven::solve();
-    // days::eight::solve();
+    days::one::solve();
+    days::two::solve();
+    days::three::solve();
+    days::four::solve();
+    days::five::solve();
+    days::six::solve();
+    days::seven::solve();
+    days::eight::solve();
     days::nine::solve();
 }
